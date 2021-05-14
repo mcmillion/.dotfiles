24d8a7e24bfc91b1155b0eafa726d25819c40fe6
Matthew McMillion <matthew@mcmillion.io>
2021-05-13 20:29:16 -0500

    Fix prettier formatting (derp)

--- nvim/.config/nvim/lua/config/formatter.lua
+++ nvim/.config/nvim/lua/config/formatter.lua
@@ -25,6 +25,6 @@ require('formatter').setup({
 vim.api.nvim_exec([[
 augroup FormatAutogroup
   autocmd!
-  autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.json,*.css,*.scss,*.graphqlFormatWrite
+  autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.json,*.css,*.scss,*.graphql FormatWrite
 augroup END
 ]], true)
