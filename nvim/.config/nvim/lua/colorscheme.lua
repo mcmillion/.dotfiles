23d9b134b32e6c6717db78be0387e75e1734afd4
Matthew McMillion <matthew@mcmillion.io>
2021-05-14 12:08:39 -0500

    Better tabline styling

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,1 +1,13 @@
+local auto = vim.api.nvim_exec
+
 vim.g.colors_name = 'onedark'
+
+-- Custom tab bar
+auto([[
+  augroup TabColors
+    autocmd!
+    autocmd ColorScheme * highlight TabLineFill guifg=#5c6370 guibg=#2d323c
+    autocmd ColorScheme * highlight TabLine guifg=#5c6370 guibg=#2d323c
+    autocmd ColorScheme * highlight TabLineSel guifg=#abb2bf guibg=#3f4452
+  augroup END
+]], true)
