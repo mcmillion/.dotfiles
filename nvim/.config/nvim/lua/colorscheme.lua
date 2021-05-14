a0c95fb9c43767ddd7ba95455eaedec231494b50
Matthew McMillion <matthew@mcmillion.io>
2021-05-14 14:40:08 -0500

    Tweak colors more

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -2,12 +2,15 @@ local auto = vim.api.nvim_exec
 
 vim.g.colors_name = 'onedark'
 
--- Custom tab bar
 auto([[
-  augroup TabColors
+  augroup ColorschemeTweaks
     autocmd!
+
     autocmd ColorScheme * highlight TabLineFill guifg=#5c6370 guibg=#2d323c
     autocmd ColorScheme * highlight TabLine guifg=#5c6370 guibg=#2d323c
     autocmd ColorScheme * highlight TabLineSel guifg=#abb2bf guibg=#3f4452
+    autocmd ColorScheme * highlight Title guifg=#5c6370 guibg=NONE
+
+    autocmd ColorScheme * hi TSNamespace guifg=#61afef guibg=NON
   augroup END
 ]], true)
