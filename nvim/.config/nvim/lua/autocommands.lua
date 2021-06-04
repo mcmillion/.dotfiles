fd0152068e9d306b66fcbec3cb83fe1dba588726
Matthew McMillion <matthew@mcmillion.io>
2021-06-03 20:41:13 -0500

    Add auto cursorline

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -7,3 +7,12 @@ auto([[
     au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
   augroup END
 ]], true)
+
+-- Cursorline in active pane
+auto([[
+  augroup CursorLine
+    autocmd!
+    au VimEnter,WinEnter,BufWinEnter,FocusGained * setlocal cursorline
+    au WinLeave,FocusLost * setlocal nocursorline
+  augroup END
+]], true)
