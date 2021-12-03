8e62fd428c990bbce98b16535bbdf218dd97c952
Matthew McMillion <matthew@mcmillion.io>
2021-12-03 10:33:16 -0600

    Fix fugutive mappings

--- nvim/.config/nvim/lua/config/fugitive.lua
+++ nvim/.config/nvim/lua/config/fugitive.lua
@@ -4,5 +4,5 @@ local noremap = utils.noremap
 noremap('n', '<leader>gs', ':vertical Git<cr>')
 noremap('n', '<Leader>gd', ':Gdiffsplit!<CR>')
 noremap('n', '<Leader>gb', ':Git blame<CR>')
-noremap('n', '<Leader>gw', ':Git write<CR>')
-noremap('n', '<Leader>gr', ':Git read<CR>')
+noremap('n', '<Leader>gw', ':Gwrite<CR>')
+noremap('n', '<Leader>gr', ':Gread<CR>')
