2db5935bd6b6daf2f1836588ac4c83c542e66460
Matthew McMillion <matthew@mcmillion.io>
2022-02-11 14:57:55 -0600

    Fix some maps that shouldn't have been silent

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -1,31 +1,32 @@
 utils = require('utils')
 local noremap = utils.noremap
+local silent_noremap = utils.noremap
 
 -- remap the leader key to <Space>
 vim.api.nvim_set_keymap('n', '<space>', '', {})
 vim.g.mapleader = ' '
 
 -- Disable ex mode
-noremap('n', 'Q', '<nop>')
+silent_noremap('n', 'Q', '<nop>')
 
 -- Disable undo text in insert mode
-noremap('i', '<c-u>', '<nop>')
+silent_noremap('i', '<c-u>', '<nop>')
 
 -- Remove last search highlight
-noremap('n', '<leader><esc>', ':nohlsearch<cr>')
+silent_noremap('n', '<leader><esc>', ':nohlsearch<cr>')
 
 -- Close quickfix and remove highlight
-noremap('n', '<leader>qx', ':cclose<cr>:noh<cr>')
+silent_noremap('n', '<leader>qx', ':cclose<cr>:noh<cr>')
 
 -- Split line
-noremap('n', 'K', 'i<cr><esc>')
+silent_noremap('n', 'K', 'i<cr><esc>')
 
 -- Remap search next to keep search in the middle of the screen
-noremap('n', 'n', 'nzzzv')
-noremap('n', 'N', 'Nzzzv')
+silent_noremap('n', 'n', 'nzzzv')
+silent_noremap('n', 'N', 'Nzzzv')
 
 -- Quicker quit
-noremap('n', 'QQ', ':q<cr>')
+silent_noremap('n', 'QQ', ':q<cr>')
 
 -- Dumb hack to format with :EslintFixAll before saving, since the BufPreWrite
 -- route breaks undo
@@ -40,27 +41,27 @@ function! MaybeFormatAndSave()
   write
 endfunction
 ]])
-noremap('n', '<leader>w', ':call MaybeFormatAndSave()<cr>')
+silent_noremap('n', '<leader>w', ':call MaybeFormatAndSave()<cr>')
 
 -- Easy change global with next
-noremap('n', 'c*', '*Ncgn')
+silent_noremap('n', 'c*', '*Ncgn')
 
 -- Window resizing
-noremap('n', '<leader><up>', ':5wincmd +<cr>')
-noremap('n', '<leader><down>', ':5wincmd -<cr>')
-noremap('n', '<leader><left>', ':5wincmd <<cr>')
-noremap('n', '<leader><right>', ':5wincmd ><cr>')
+silent_noremap('n', '<leader><up>', ':5wincmd +<cr>')
+silent_noremap('n', '<leader><down>', ':5wincmd -<cr>')
+silent_noremap('n', '<leader><left>', ':5wincmd <<cr>')
+silent_noremap('n', '<leader><right>', ':5wincmd ><cr>')
 
 -- Panes / Tabs
-noremap('n', '<leader>-', ':vsplit %:h<cr>')
-noremap('n', '<leader>_', ':split %:h<cr>')
-noremap('n', '<leader>=', ':tab split %:h<cr>')
+silent_noremap('n', '<leader>-', ':vsplit %:h<cr>')
+silent_noremap('n', '<leader>_', ':split %:h<cr>')
+silent_noremap('n', '<leader>=', ':tab split %:h<cr>')
 
 -- Delete selected to text to void and paste default register
-noremap('v', '<leader>p', '"_dP')
+silent_noremap('v', '<leader>p', '"_dP')
 
 -- Yank to end of line because consistency makes sense
-noremap('n', 'Y', 'y$')
+silent_noremap('n', 'Y', 'y$')
 
 -- Substitute current word
 noremap('n', '<leader>s', ':s/<c-r><c-w>//g<left><left>')
@@ -68,4 +69,4 @@ noremap('n', '<leader>S', ':%s/<c-r><c-w>//g<left><left>')
 noremap('x', '<leader>s', ':s///g<left><left><left>')
 
 -- Popup Terminal
-noremap('n', '<leader>`', ':FloatermNew --width=0.6 --height=0.6 --autoclose=1<cr>')
+silent_noremap('n', '<leader>`', ':FloatermNew --width=0.6 --height=0.6 --autoclose=1<cr>')
--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -1,6 +1,10 @@
 local utils = {}
 
 function utils.noremap(mode, map, command)
+  vim.api.nvim_set_keymap(mode, map, command, { noremap = true })
+end
+
+function utils.silent_noremap(mode, map, command)
   vim.api.nvim_set_keymap(mode, map, command, { noremap = true, silent = true })
 end
 
