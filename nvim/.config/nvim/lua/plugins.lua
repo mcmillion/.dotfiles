c29e27d035fb53335aaf2f8bf66ab1af3696ea0d
Matthew McMillion <matthew@mcmillion.io>
2022-03-06 17:37:58 -0600

    Re-add autosession

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -81,6 +81,7 @@ return require('packer').startup(function()
   use 'tpope/vim-surround'
   use 'tpope/vim-unimpaired'
   use 'tpope/vim-vinegar'
+  use 'rmagatti/auto-session'
   use 'editorconfig/editorconfig-vim'
   use {
     'davidgranstrom/nvim-markdown-preview',
