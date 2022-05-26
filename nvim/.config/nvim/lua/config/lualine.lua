1f5005982d8c5ae198b179b7112be6f0c39994cb
Matthew McMillion <matthew@mcmillion.io>
2022-05-26 10:27:39 -0500

    Also show full path in inactive statusline

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -32,7 +32,7 @@ return require('lualine').setup({
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{ 'filename', path = 1 }},
     lualine_x = {},
     lualine_y = {},
     lualine_z = {}
