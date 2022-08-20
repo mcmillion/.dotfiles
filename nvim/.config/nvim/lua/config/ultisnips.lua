5a56551fafc96384a8765b896e26b9c2d907eb45
Matthew McMillion <matthew@mcmillion.io>
2022-08-20 16:02:51 -0500

    whatever, I need to move to luasnips at some point probably

--- nvim/.config/nvim/lua/config/ultisnips.lua
+++ nvim/.config/nvim/lua/config/ultisnips.lua
@@ -1,6 +1,5 @@
 vim.g.UltiSnipsExpandTrigger = "<TAB>"
 vim.g.UltiSnipsJumpForwardTrigger="<TAB>"
-vim.g.UltiSnipsJumpBackwardTrigger="<S-TAB>"
 vim.g.UltiSnipsEditSplit="vertical"
 
 -- Automatically reload snippets when edited
