f4dc7a2b7a3e37ebfc106ddb31552e25698297c9
Matthew McMillion <matthew@mcmillion.io>
2022-11-07 20:56:21 -0600

    flerp

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -12,10 +12,10 @@ return require('packer').startup(function()
     run = ':TSUpdate',
     config = function() require('config.treesitter') end
   }
-  use {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    config = function() require('config.treesitter-textobjects') end
-  }
+  -- use {
+  --   'nvim-treesitter/nvim-treesitter-textobjects',
+  --   config = function() require('config.treesitter-textobjects') end
+  -- }
   use {
     'norcalli/nvim-colorizer.lua',
     config = function() require('config.colorizer') end
