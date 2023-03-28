e32691c00aac3b133e274afbe7ebac7c8f84b348
Matthew McMillion <matthew@mcmillion.io>
2023-03-28 09:55:07 -0500

    Actually set up nvim-spider

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -26,7 +26,10 @@ return require('packer').startup(function()
   }
 
   -- Movement
-  use { "chrisgrieser/nvim-spider" }
+  use {
+    "chrisgrieser/nvim-spider",
+    config = function() require('config.nvim-spider') end
+  }
   use {
     'nvim-treesitter/nvim-treesitter-textobjects',
     config = function() require('config.treesitter-textobjects') end
