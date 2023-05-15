f8a0c127001cc7cd09711ecba9bb36d42bae3015
Matthew McMillion <matthew@mcmillion.io>
2023-05-15 14:59:56 -0500

    Fix auto-session not loading

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -134,7 +134,10 @@ return require('packer').startup(function()
   use 'tpope/vim-surround'
   use 'tpope/vim-unimpaired'
   use 'tpope/vim-vinegar'
-  use 'rmagatti/auto-session'
+  use {
+    'rmagatti/auto-session',
+    config = function() require('config.auto-session') end
+  }
   use 'editorconfig/editorconfig-vim'
   use {
     'davidgranstrom/nvim-markdown-preview',
