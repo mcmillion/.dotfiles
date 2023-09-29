5678fb2db2cf9366fb6ee4f13760b477733b1083
Matthew McMillion <matthew@mcmillion.io>
2023-09-29 12:26:48 -0500

    Fix indent blankline thickness

--- nvim/.config/nvim/lua/config/indent-blankline.lua
+++ nvim/.config/nvim/lua/config/indent-blankline.lua
@@ -1,4 +1,6 @@
 require("ibl").setup({
+  indent = { char = "┆" },
+  scope = { char = "│" },
   filetype_exclude = { 'man', 'help', 'startify', 'dashboard', 'packer', 'neogitstatus', 'markdown' },
   buftype_exclude = { 'terminal' },
 })
