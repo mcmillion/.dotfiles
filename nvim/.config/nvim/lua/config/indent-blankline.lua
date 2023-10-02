df151c12f457b8a266e7e2261fdadcff781b2c12
Matthew McMillion <matthew@mcmillion.io>
2023-10-02 08:45:18 -0500

    Fix indent-blankline again

--- nvim/.config/nvim/lua/config/indent-blankline.lua
+++ nvim/.config/nvim/lua/config/indent-blankline.lua
@@ -1,6 +1,4 @@
 require("ibl").setup({
   indent = { char = '▏'},
   scope = { char = '▎'},
-  filetype_exclude = { 'man', 'help', 'startify', 'dashboard', 'packer', 'neogitstatus', 'markdown' },
-  buftype_exclude = { 'terminal' },
 })
