cc9b793cec37265144b351bffe861e46c0d3dd67
Matthew McMillion <matthew@mcmillion.io>
2024-12-02 00:16:15 -0600

    Setup DenoLS and prevent it from clashing with TS

--- nvim/.config/nvim/lua/config/lspconfig-tailwindcss.lua
+++ nvim/.config/nvim/lua/config/lspconfig-tailwindcss.lua
@@ -1,10 +1,9 @@
+local lspconfig = require("lspconfig")
+
 common = require("config/lspconfig-common")
 local on_attach = common.on_attach
 
-require("lspconfig").tailwindcss.setup({
+lspconfig.tailwindcss.setup({
   on_attach = on_attach,
-  -- filetypes = {
-  --   'css',
-  --   'scss',
-  -- },
+  root_dir = lspconfig.util.root_pattern("package.json"),
 })
--- nvim/.config/nvim/lua/config/lspconfig-tsserver.lua
+++ nvim/.config/nvim/lua/config/lspconfig-tsserver.lua
@@ -1,6 +1,10 @@
+local lspconfig = require("lspconfig")
+
 common = require("config/lspconfig-common")
 local on_attach = common.on_attach
 
-require("lspconfig").ts_ls.setup({
+lspconfig.ts_ls.setup({
   on_attach = on_attach,
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false,
 })
