c440096a41e81d354195772d468a52c276e71d0a
Matthew McMillion <matthew@mcmillion.io>
2025-03-20 10:23:39 -0500

    Lots of cleanup

--- nvim/.config/nvim/after/ftplugin/ruby.lua
+++ nvim/.config/nvim/after/ftplugin/ruby.lua
@@ -1,4 +1,6 @@
-utils = require('utils')
-local noremap = utils.noremap
-
-noremap('n', '<c-b>', ":FloatermNew --width=0.8 --height=0.8 bundle exec rspec<cr>")
+vim.keymap.set(
+  "n",
+  "<c-b>",
+  ":FloatermNew --width=0.8 --height=0.8 bundle exec rspec<cr>",
+  { noremap = true, silent = true }
+)
--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -1,4 +1,6 @@
-utils = require('utils')
-local noremap = utils.noremap
-
-noremap('n', '<c-b>', ":FloatermNew --autoclose=0 --width=0.8 --height=0.8 cargo run<cr>")
+vim.keymap.set(
+  "n",
+  "<c-b>",
+  ":FloatermNew --autoclose=0 --width=0.8 --height=0.8 cargo run<cr>",
+  { noremap = true, silent = true }
+)
