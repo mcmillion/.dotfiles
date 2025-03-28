d2cd75649e91062901e08cd96b17bcc05ac7ab20
Matthew McMillion <matthew@mcmillion.io>
2025-03-28 18:07:44 -0500

    Stuff

--- nvim/.config/nvim/lua/config/settings.lua
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -8,7 +8,7 @@ vim.o.winborder = "single"
 -- Use termgui colors and dark mode
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.cmd([[colorscheme onedark]])
+vim.cmd.colorscheme("onedark")
 
 -- Disable backups and swaps
 vim.o.backup = false
--- nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,3 +1,7 @@
 return {
   "norcalli/nvim-colorizer.lua",
+
+  config = function()
+    require("colorizer").setup()
+  end,
 }
