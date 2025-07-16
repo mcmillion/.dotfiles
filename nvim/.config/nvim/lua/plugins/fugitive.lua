51645c53982e2829200f4bef33d10056f7eb4bc7
Matthew McMillion <matthew@mcmillion.io>
2025-07-16 11:23:26 -0500

    Maybe fix fugitive

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,5 +1,6 @@
 return {
   "tpope/vim-fugitive",
+  lazy = false,
 
   config = function()
     vim.keymap.set("n", "<leader>gs", ":vertical Git<cr>", { noremap = true, silent = true })
@@ -7,5 +8,6 @@ return {
     vim.keymap.set("n", "<Leader>gb", ":Git blame<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<Leader>gw", ":Gwrite<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<Leader>gr", ":Gread<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "dp", ":diffput<CR>", { buffer = true })
   end,
 }
