81a3fc108fdd8ff5c457728e7d7c5803d539da3d
Matthew McMillion <matthew@mcmillion.io>
2025-11-15 13:14:46 -0600

    Add GSL filetype configuration for Neovim
    - Configure tab width and shift width to 4 spaces for GSL files
    - Set custom listchars for tabs and trailing whitespace

---
+++ nvim/.config/nvim/after/ftplugin/gsl.lua
@@ -0,0 +1,10 @@
+vim.opt_local.listchars = {
+  tab = "  ",
+  trail = "·",
+  extends = "→",
+  precedes = "←",
+}
+
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
