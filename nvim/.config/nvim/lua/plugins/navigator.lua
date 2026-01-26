1f029b9e4cdaa181ef5156f3da1f2ebb55ce8798
Matthew McMillion <matt@mlm.dev>
2026-01-26 12:35:52 -0600

    Make Navigator keybindings silent

--- nvim/.config/nvim/lua/plugins/navigator.lua
+++ nvim/.config/nvim/lua/plugins/navigator.lua
@@ -5,9 +5,9 @@ return {
   opts = {},
 
   keys = {
-    { "<c-h>", "<CMD>NavigatorLeft<CR>" },
-    { "<c-k>", "<CMD>NavigatorUp<CR>" },
-    { "<c-l>", "<CMD>NavigatorRight<CR>" },
-    { "<c-j>", "<CMD>NavigatorDown<CR>" },
+    { "<c-h>", "<CMD>NavigatorLeft<CR>", silent = true },
+    { "<c-k>", "<CMD>NavigatorUp<CR>", silent = true },
+    { "<c-l>", "<CMD>NavigatorRight<CR>", silent = true },
+    { "<c-j>", "<CMD>NavigatorDown<CR>", silent = true },
   },
 }
